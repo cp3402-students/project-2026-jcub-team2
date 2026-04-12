52cb4f8a2007de37c0667fe48ac89052b86f0e9b
Gurjas-R <gurjas.ranu@my.jcu.edu.au>
2026-04-12 20:35:15 +1000

    Load Google Fonts via functions.php

--- wp-content/themes/kctennisblast-theme/functions.php
+++ wp-content/themes/kctennisblast-theme/functions.php
@@ -207,8 +207,8 @@ add_action( 'init', 'kctennisblast_theme_register_menus' );
 # Enqueue font
 function kctennisblast_fonts() {
   wp_enqueue_style(
-    'kctennisblast-poppins',
-    'https://fonts.googleapis.com/css2?family=Poppins:ital,wght@0,100;0,200;0,300;0,400;0,500;0,600;0,700;0,800;0,900;1,100;1,200;1,300;1,400;1,500;1,600;1,700;1,800;1,900&display=swap',
+    'kctennisblast-google-fonts',
+    'https://fonts.googleapis.com/css2?family=Montserrat:ital,wght@0,100..900;1,100..900&family=Poppins:ital,wght@0,100;0,200;0,300;0,400;0,500;0,600;0,700;0,800;0,900;1,100;1,200;1,300;1,400;1,500;1,600;1,700;1,800;1,900&display=swap',
     array(),
     null
   );
