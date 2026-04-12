c17a8cc8267ede11fed629f4d1ff3a5363a078fe
Gurjas-R <gurjas.ranu@my.jcu.edu.au>
2026-04-12 19:29:14 +1000

    Remove bloat

--- wp-content/themes/kctennisblast-theme/404.php
+++ wp-content/themes/kctennisblast-theme/404.php
@@ -18,30 +18,9 @@ get_header();
 			</header><!-- .page-header -->
 
 			<div class="page-content">
-				<p><?php esc_html_e( 'It looks like nothing was found at this location. Maybe try one of the links below or a search?', 'kctennisblast-theme' ); ?></p>
-
-					<?php
-					get_search_form();
-
-					the_widget( 'WP_Widget_Recent_Posts' );
-					?>
-
-					<div class="widget widget_categories">
-						<h2 class="widget-title"><?php esc_html_e( 'Most Used Categories', 'kctennisblast-theme' ); ?></h2>
-						<ul>
-							<?php
-							wp_list_categories(
-								array(
-									'orderby'    => 'count',
-									'order'      => 'DESC',
-									'show_count' => 1,
-									'title_li'   => '',
-									'number'     => 10,
-								)
-							);
-							?>
-						</ul>
-					</div><!-- .widget -->
+				<p><?php esc_html_e( 'It looks like nothing was found at this location. Maybe try a search?', 'kctennisblast-theme' ); ?></p>
+				<?php get_search_form(); ?>
+			</div><!-- .page-content -->
 
 					<?php
 					/* translators: %1$s: smiley */
